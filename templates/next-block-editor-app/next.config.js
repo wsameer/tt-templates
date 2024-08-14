cb86b252e7be1f75cce492097ffc8ff84d3f20c8
Nick the Sick <nicholas.perez@tiptap.dev>
2024-08-14 09:05:06 +0200

    fix: allow installing from the root repo

--- templates/next-block-editor-app/next.config.js
+++ templates/next-block-editor-app/next.config.js
@@ -3,17 +3,10 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   webpack(config, { isServer }) {
     if (!isServer) {
       // Ensure that all imports of 'yjs' resolve to the same instance
-      config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs')
+      config.resolve.alias['yjs'] = path.resolve(__dirname, '../../node_modules/yjs')
     }
     config.module.rules.push({
       test: /\.svg$/,
