969424f7ed00daa8cf05171b3c40eedf68d3d3e6
Nick the Sick <nicholas.perez@tiptap.dev>
2024-08-30 09:14:50 +0200

    fix: make menu show is active state properly

--- templates/next-block-editor-app/src/components/menus/TextMenu/hooks/useTextmenuContentTypes.ts
+++ templates/next-block-editor-app/src/components/menus/TextMenu/hooks/useTextmenuContentTypes.ts
@@ -1,10 +1,12 @@
 import { useMemo } from 'react'
-import { Editor } from '@tiptap/react'
+import deepEql from 'fast-deep-equal'
+import { Editor, useEditorState } from '@tiptap/react'
 import { ContentPickerOptions } from '../components/ContentTypePicker'
 
 export const useTextmenuContentTypes = (editor: Editor) => {
-  const options = useMemo<ContentPickerOptions>(() => {
-    return [
+  return useEditorState({
+    editor,
+    selector: ctx => [
       {
         type: 'category',
         label: 'Hierarchy',
@@ -12,41 +14,44 @@ export const useTextmenuContentTypes = (editor: Editor) => {
       },
       {
         icon: 'Pilcrow',
-        onClick: () => editor.chain().focus().lift('taskItem').liftListItem('listItem').setParagraph().run(),
+        onClick: () => ctx.editor.chain().focus().lift('taskItem').liftListItem('listItem').setParagraph().run(),
         id: 'paragraph',
-        disabled: () => !editor.can().setParagraph(),
+        disabled: () => !ctx.editor.can().setParagraph(),
         isActive: () =>
-          editor.isActive('paragraph') &&
-          !editor.isActive('orderedList') &&
-          !editor.isActive('bulletList') &&
-          !editor.isActive('taskList'),
+          ctx.editor.isActive('paragraph') &&
+          !ctx.editor.isActive('orderedList') &&
+          !ctx.editor.isActive('bulletList') &&
+          !ctx.editor.isActive('taskList'),
         label: 'Paragraph',
         type: 'option',
       },
       {
         icon: 'Heading1',
-        onClick: () => editor.chain().focus().lift('taskItem').liftListItem('listItem').setHeading({ level: 1 }).run(),
+        onClick: () =>
+          ctx.editor.chain().focus().lift('taskItem').liftListItem('listItem').setHeading({ level: 1 }).run(),
         id: 'heading1',
-        disabled: () => !editor.can().setHeading({ level: 1 }),
-        isActive: () => editor.isActive('heading', { level: 1 }),
+        disabled: () => !ctx.editor.can().setHeading({ level: 1 }),
+        isActive: () => ctx.editor.isActive('heading', { level: 1 }),
         label: 'Heading 1',
         type: 'option',
       },
       {
         icon: 'Heading2',
-        onClick: () => editor.chain().focus().lift('taskItem').liftListItem('listItem').setHeading({ level: 2 }).run(),
+        onClick: () =>
+          ctx.editor.chain().focus().lift('taskItem').liftListItem('listItem').setHeading({ level: 2 }).run(),
         id: 'heading2',
-        disabled: () => !editor.can().setHeading({ level: 2 }),
-        isActive: () => editor.isActive('heading', { level: 2 }),
+        disabled: () => !ctx.editor.can().setHeading({ level: 2 }),
+        isActive: () => ctx.editor.isActive('heading', { level: 2 }),
         label: 'Heading 2',
         type: 'option',
       },
       {
         icon: 'Heading3',
-        onClick: () => editor.chain().focus().lift('taskItem').liftListItem('listItem').setHeading({ level: 3 }).run(),
+        onClick: () =>
+          ctx.editor.chain().focus().lift('taskItem').liftListItem('listItem').setHeading({ level: 3 }).run(),
         id: 'heading3',
-        disabled: () => !editor.can().setHeading({ level: 3 }),
-        isActive: () => editor.isActive('heading', { level: 3 }),
+        disabled: () => !ctx.editor.can().setHeading({ level: 3 }),
+        isActive: () => ctx.editor.isActive('heading', { level: 3 }),
         label: 'Heading 3',
         type: 'option',
       },
@@ -57,34 +62,32 @@ export const useTextmenuContentTypes = (editor: Editor) => {
       },
       {
         icon: 'List',
-        onClick: () => editor.chain().focus().toggleBulletList().run(),
+        onClick: () => ctx.editor.chain().focus().toggleBulletList().run(),
         id: 'bulletList',
-        disabled: () => !editor.can().toggleBulletList(),
-        isActive: () => editor.isActive('bulletList'),
+        disabled: () => !ctx.editor.can().toggleBulletList(),
+        isActive: () => ctx.editor.isActive('bulletList'),
         label: 'Bullet list',
         type: 'option',
       },
       {
         icon: 'ListOrdered',
-        onClick: () => editor.chain().focus().toggleOrderedList().run(),
+        onClick: () => ctx.editor.chain().focus().toggleOrderedList().run(),
         id: 'orderedList',
-        disabled: () => !editor.can().toggleOrderedList(),
-        isActive: () => editor.isActive('orderedList'),
+        disabled: () => !ctx.editor.can().toggleOrderedList(),
+        isActive: () => ctx.editor.isActive('orderedList'),
         label: 'Numbered list',
         type: 'option',
       },
       {
         icon: 'ListTodo',
-        onClick: () => editor.chain().focus().toggleTaskList().run(),
+        onClick: () => ctx.editor.chain().focus().toggleTaskList().run(),
         id: 'todoList',
-        disabled: () => !editor.can().toggleTaskList(),
-        isActive: () => editor.isActive('taskList'),
+        disabled: () => !ctx.editor.can().toggleTaskList(),
+        isActive: () => ctx.editor.isActive('taskList'),
         label: 'Todo list',
         type: 'option',
       },
-    ]
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [editor, editor.state])
-
-  return options
+    ],
+    equalityFn: deepEql,
+  })
 }
