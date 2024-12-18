629832e9bbf108061e1184c4744cf98a74c1d4e0
Dominik Biedebach <dominik.biedebach@ueber.io>
2024-12-18 23:12:58 +0100

    fix: remove javascript xss injections from link extension

--- templates/next-block-editor-app/src/extensions/Link/Link.ts
+++ templates/next-block-editor-app/src/extensions/Link/Link.ts
@@ -7,10 +7,26 @@ export const Link = TiptapLink.extend({
   inclusive: false,
 
   parseHTML() {
-    return [{ tag: 'a[href]:not([data-type="button"]):not([href *= "javascript:" i])' }]
+    return [
+      {
+        tag: 'a[href]:not([data-type="button"]):not([href *= "javascript:" i])',
+        getAttrs: element => {
+          // check if link starts with javascript:
+          if (element.getAttribute('href')?.toLowerCase().startsWith('javascript:')) {
+            return false
+          }
+
+          return null
+        },
+      },
+    ]
   },
 
   renderHTML({ HTMLAttributes }) {
+    if (HTMLAttributes.href?.toLowerCase().startsWith('javascript:')) {
+      return ['a', mergeAttributes(this.options.HTMLAttributes, { ...HTMLAttributes, href: '' }, { class: 'link' }), 0]
+    }
+
     return ['a', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, { class: 'link' }), 0]
   },
 
